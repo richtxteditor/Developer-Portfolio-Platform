378707c9cc703c345eb3bc1c4fd0353eb0cfbebd
John <4448701+jrmol@users.noreply.github.com>
2024-05-04 19:34:57 -0400

    landing page tailwind work

--- django_portion/static/django_portion/base.js
+++ django_portion/static/django_portion/base.js
@@ -63,3 +63,29 @@ const cspNonceMeta = document.querySelector('meta[name="csp-nonce"]');
 if (cspNonceMeta) {
 	window.CSP_NONCE = cspNonceMeta.content;
 }
+
+var swiper = new Swiper(".centered-slide-carousel", {
+	centeredSlides: true,
+	paginationClickable: true,
+	loop: true,
+	spaceBetween: 30,
+	slideToClickedSlide: true,
+	pagination: {
+		el: ".centered-slide-carousel .swiper-pagination",
+		clickable: true,
+	},
+	breakpoints: {
+		1920: {
+			slidesPerView: 4,
+			spaceBetween: 30,
+		},
+		1028: {
+			slidesPerView: 2,
+			spaceBetween: 10,
+		},
+		990: {
+			slidesPerView: 1,
+			spaceBetween: 0,
+		},
+	},
+});
