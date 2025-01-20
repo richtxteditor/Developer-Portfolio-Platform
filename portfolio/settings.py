2c1696953926cca7ef97aff60764ffd0a0e6494b
Richie <johnrichardmolina@gmail.com>
2025-01-20 16:23:37 -0500

    dependcy updates

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -15,8 +15,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ['174.138.70.92', 'johndev.io', 'www.johndev.io', '127.0.0.1']
-
+ALLOWED_HOSTS = ['localhost', '172.26.139.35', '[::1]', 'johndev.io', 'www.johndev.io', '127.0.0.1']
 
 INSTALLED_APPS = [
     "webpack_loader",
